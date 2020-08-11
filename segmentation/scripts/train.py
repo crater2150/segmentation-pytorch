feb12aaec9a807d8c88390dc1727f6c7911f604b
Alexander Hartelt <hartelt.alexander@gmail.com>
2020-08-11 12:59:10 +0200

    added odsrunner

--- segmentation/scripts/train.py
+++ segmentation/scripts/train.py
@@ -4,8 +4,8 @@ from os import path
 from typing import List
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-
+from segmentation.settings import PredictorSettings
+from segmentation.optimizer import Optimizers
 def dir_path(string):
     if path.isdir(string):
         return string
@@ -17,7 +17,6 @@ def main():
     from segmentation.network import TrainSettings, dirs_to_pandaframe, load_image_map_from_file, MaskSetting, MaskType, PCGTSVersion, XMLDataset, Network, compose, MaskGenerator, MaskDataset
     from segmentation.settings import Architecture
     from segmentation.modules import ENCODERS
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-L", "--l-rate", type=float, default=1e-4,
                         help="set learning rate")
@@ -25,7 +24,7 @@ def main():
                         help="target directory for model and logs")
     parser.add_argument("--load", type=str, default=None,
                         help="load an existing model and continue training")
-    parser.add_argument("-E", "--n-epoch", type=int, default=100,
+    parser.add_argument("-E", "--n_epoch", type=int, default=15,
                         help="number of epochs")
     parser.add_argument("--data-augmentation", action="store_true",
                         help="Enable data augmentation")
@@ -35,7 +34,7 @@ def main():
     parser.add_argument("--test_input", type=dir_path, nargs="*", default=[], help="Path to folder(s) containing test images")
     parser.add_argument("--test_mask", type=dir_path, nargs="+", default=[], help="Path to folder(s) containing test xmls")
 
-    parser.add_argument("--color-map", dest="map", type=str, required=True,
+    parser.add_argument("--color_map", dest="map", type=str, required=True,
                         help="path to color map to load")
     parser.add_argument('--architecture',
                         default=Architecture.UNET,
@@ -49,11 +48,17 @@ def main():
                         choices=ENCODERS,
                         nargs='?',
                         help='Network architecture to use for training')
+    parser.add_argument('--optimizer', default="adam", const="adam", nargs='?',
+                        choices=[x.value for x in list(Optimizers)])
+    parser.add_argument('--batch_accumulation', default=1, type=int)
+    parser.add_argument('--eval', action="store_true", help="Starts evaluation on test set after training")
+
 
     args = parser.parse_args()
-
     train = dirs_to_pandaframe(args.train_input, args.train_mask)
-    test = dirs_to_pandaframe(args.test_input, args.train_mask) if len(args.test_input) > 0 else train
+    test = dirs_to_pandaframe(args.test_input, args.test_mask)
+    test = test if len(test) > 0 else train
+
     map = load_image_map_from_file(args.map)
     from segmentation.dataset import base_line_transform
 
@@ -66,9 +71,18 @@ def main():
 
     setting = TrainSettings(CLASSES=len(map), TRAIN_DATASET=train_dataset, VAL_DATASET=test_dataset,
                             OUTPUT_PATH=args.output,
-                            MODEL_PATH=args.load)
+                            MODEL_PATH=args.load, EPOCHS=args.n_epoch,
+                            OPTIMIZER=Optimizers(args.optimizer), BATCH_ACCUMULATION=args.batch_accumulation, ENCODER=args.encoder,
+                            ARCHITECTURE=Architecture(args.architecture))
+
     trainer = Network(setting, color_map=map)
     trainer.train()
+    if args.eval:
+        setting = PredictorSettings(PREDICT_DATASET=test_dataset, MODEL_PATH=args.output+".torch")
+        predictor = Network(setting, color_map=map)
+        accuracy, loss = predictor.eval()
+        print("EXPERIMENT_OUT="+str(accuracy)+","+str(loss))
+
 
 
 if __name__ == "__main__":
